367320554a6396ba5ec4844cba219bc20114c611
ying1488 <yingecteach@gmail.com>
2024-03-08 11:44:28 +1100

    react

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import qgl from 'graphql-tag';
 
 class SongCreate extends Component {
 
@@ -7,11 +8,18 @@ class SongCreate extends Component {
 
     this.state = { title: '' };
   }
+
+  onSubmit(event) {
+    event.preventDefault();
+
+
+  }
+
   render() {
     return (
       <div>
         <h3>Create a New Song</h3>
-        <form>
+        <form onSubmit={this.onSubmit.bind(this)}>
           <label>Song Title:</label>
           <input
             onChange={event => this.setState({ title: event.target.value })}
@@ -22,5 +30,11 @@ class SongCreate extends Component {
     );
   }
 }
+
+const mutation = gql`
+mutation{
+  addSong(title: )
+}
+`;
 
 export default SongCreate;
