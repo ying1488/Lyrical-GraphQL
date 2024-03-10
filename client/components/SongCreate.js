12614b3b38a06a4486fc4fec56d00bb4ca28fc96
ying1488 <yingecteach@gmail.com>
2024-03-10 22:22:29 +1100

    add LyricCreate updated mutation

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,23 +1,29 @@
 import React, { Component } from "react";
-import qgl from 'graphql-tag';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+import { Link, hashHistory } from 'react-router';
+import query from '../queries/fetchSongs';
 
 class SongCreate extends Component {
 
   constructor(props) {
     super(props);
-
     this.state = { title: '' };
   }
 
   onSubmit(event) {
     event.preventDefault();
-
-
+    //actual mutation
+    this.props.mutate({
+      variables: { title: this.state.title },
+      refetchQueries: [{ query }]
+    }).then(() => hashHistory.push('/'));
   }
 
   render() {
     return (
       <div>
+        <Link to="/">Back</Link>
         <h3>Create a New Song</h3>
         <form onSubmit={this.onSubmit.bind(this)}>
           <label>Song Title:</label>
@@ -32,9 +38,11 @@ class SongCreate extends Component {
 }
 
 const mutation = gql`
-mutation{
-  addSong(title: )
+mutation AddSong($title:String){
+  addSong(title: $title){
+    title
+  }
 }
 `;
 
-export default SongCreate;
+export default graphql(mutation)(SongCreate);
--- client/index.js
+++ client/index.js
@@ -1,3 +1,4 @@
+import './style/style.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, hashHistory, IndexRoute } from 'react-router';
@@ -7,15 +8,19 @@ import { ApolloProvider } from 'react-apollo';
 import App from './components/App';
 import SongList from './components/SongList';
 import SongCreate from './components/SongCreate';
+import SongDetail from './components/SongDetail';
 
-const client = new ApolloClient({});
+const client = new ApolloClient({
+  dataIdFromObject: o => o.id
+});
 
 const Root = () => {
   return <ApolloProvider client={client}>
     <Router history={hashHistory}>
       <Route path="/" component={App}>
         <IndexRoute component={SongList} />
-        <Route path="song/new" component={SongCreate} />
+        <Route path="songs/new" component={SongCreate} />
+        <Route path="songs/:id" component={SongDetail} />
       </Route>
     </Router>
   </ApolloProvider>
